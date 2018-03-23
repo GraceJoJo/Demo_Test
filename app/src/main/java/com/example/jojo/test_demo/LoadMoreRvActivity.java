f88be4139689d6ed989376da62d2b19357dac975
JOJO <18510829974@163.com>
2018-03-23 17:28:39 +0800

    开发中用到的小Demo

--- app/src/main/java/com/example/jojo/test_demo/LoadMoreRvActivity.java
+++ app/src/main/java/com/example/jojo/test_demo/LoadMoreRvActivity.java
@@ -2,7 +2,10 @@ package com.example.jojo.test_demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 
 
 /**
@@ -19,5 +22,47 @@ public class LoadMoreRvActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_more_rv);
         recyclerview = findViewById(R.id.recyclerview);
+        recyclerview.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerView.Adapter mAdapter = new RecyclerView.Adapter() {
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                return null;
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 0;
+            }
+        };
+        recyclerview.setAdapter(mAdapter);
+
+    }
+    class MyAdapter extends RecyclerView.Adapter<MyViewHolder>{
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+
+     private class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(View itemView) {
+            super(itemView);
+        }
     }
 }
